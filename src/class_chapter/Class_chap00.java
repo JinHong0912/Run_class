05483c20297e51e4a22b75b8eaf038de5224f853
JinHong <jin.ryu0912@gmail.com>
2021-10-10 20:50:11 +0900

    init : Class 프로젝트 초기화

---
+++ src/class_chapter/Class_chap00.java
@@ -0,0 +1,10 @@
+package class_chapter;
+
+public class Class_chap00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello word!!");
+	}
+
+}
