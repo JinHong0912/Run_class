98dfb7faba2af5ac3b159f92f1048cb96c935db7
JinHong <jin.ryu0912@gmail.com>
2021-10-11 21:44:42 +0900

    mistake : 작업을 master에서 작업을 함

--- src/class_chapter/Class_chap031.java
+++ src/class_chapter/Class_chap031.java
@@ -3,7 +3,33 @@ package class_chapter;
 public class Class_chap031 {
 
 	public static void main(String[] args) {
-		//부호 연산자 + -
+		// 산술 연산자
+		//예제 3-5 ch3/OperatorEx5
+//		
+//		int a = 10;
+//		int b = 4;
+//		
+//		System.out.printf("%d + %d = %d%n", a , b, a + b );
+//		System.out.printf("%d - %d = %d%n", a , b, a - b );
+//		System.out.printf("%d * %d = %d%n", a , b, a * b );
+//		System.out.printf("%d / %d = %d%n", a , b, a / b );
+//		System.out.printf("%d / %f = %f%n", a , (float) b, a / (float)b );
+		
+		// 산술 연산자
+		//예제 3-6 ch3/OperatorEx6
+		byte a = 10;
+		byte b = 20;
+		//byte c = a + b;
+		
+		byte c = (byte) (a + b);
+		System.out.println(c);
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
