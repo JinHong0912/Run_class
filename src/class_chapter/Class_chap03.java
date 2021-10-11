5804c40b661b1f356196e6c339a7bcf8447f77bd
JinHong <jin.ryu0912@gmail.com>
2021-10-11 21:24:43 +0900

    add : 자바의 정석 Chapter 03 연산자 실습

---
+++ src/class_chapter/Class_chap03.java
@@ -0,0 +1,50 @@
+package class_chapter;
+
+public class Class_chap03 {
+
+	public static void main(String[] args) {
+//		//예제 3-1 ch3/OperatorEx1
+//		int i = 5;
+//		i++; // i=i+1; 과 같은 의미이다. ++i;로 바꿔써도 결과는 같다.
+//		System.out.println(i);
+//		
+//		i=5;
+//		++i;
+//		System.out.println(i);
+
+//		//예제 3-2 ch3/OperatorEx2
+//		int i = 5, j = 0;
+//		
+//		j = i++;
+//		System.out.println("j=i++; 실행 후 , i="+i+", j="+j);
+//		
+//		i=5;  // 결과를 비교하기 위해 , i와 j의 값을 다시 5와 0으로 변경
+//		j=0;
+//	
+//		j= ++i;
+//		System.out.println("j=i++; 실행 후 , i="+i+", j="+j);
+//		
+
+		
+//		//예제 3-3 ch3/OperatorEx3
+//		int i = 5, j = 5;
+//		
+//		System.out.println(i++);
+//		System.out.println(++j);
+//		System.out.println("i = "+ i +", j=" + j);
+//		
+		//부호 연산자 
+		//예제 3-4 ch3/OperatorEx4
+		int i = -10;
+		i = +i;
+		
+		System.out.println(i);
+		
+		i = -10;
+		i= -i;
+		System.out.println(i);
+		
+	
+	}
+	
+}
---
+++ src/class_chapter/Class_chap031.java
@@ -0,0 +1,11 @@
+package class_chapter;
+
+public class Class_chap031 {
+
+	public static void main(String[] args) {
+		//부호 연산자 + -
+		
+		
+		
+	}
+}
