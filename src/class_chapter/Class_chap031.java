0483c759f8c26b9b254f2cd117bd3b7dc757430d
JinHong <jin.ryu0912@gmail.com>
2021-10-19 23:12:01 +0900

    add : 3장 수정

--- src/class_chapter/Class_chap031.java
+++ src/class_chapter/Class_chap031.java
@@ -57,7 +57,25 @@ public class Class_chap031 {
 //		'0' = 48
 //		'2' = 50
 //		
+		//예제 3-12 ch3/OperatorEx12
+		char c1 = 'a'; //c1에는 문자  'a' 의 코드값인 97이 저장 된다.
+		char c2 = c1 ; //c1에 저장되어 있는 값이 c2에 저장된다.
+		char c3 = ' '; //c3를 공백으로 초기화 한다.
+		
+		int i = c1 + 1; // 'a' + 1 -> 97 +1 -> 98
+		
+		c3 = (char)(c1 + 1);
+		c2++;
+		c2++;
+		
+		System.out.println("i=" + i);
+		System.out.println("c2=" + c2);
+		System.out.println("c3=" + c3);
 		
 		
+//		i=98
+//		c2=c
+//		c3=b
+
 	}
 }
